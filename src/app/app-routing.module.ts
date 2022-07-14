17ccce92da78f68680657c70643a61ead8b00a4d
Michael Olasanoye <cerebrum@Cerebrum.local>
2022-07-14 14:45:36 +0100

    Files restructure and conflict merged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user/dashboard', pathMatch: 'full' },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
