d88d286a59562b962299374359e56b9b1517195f
shedx3 <sheddybobo44@gmail.com>
2022-08-14 22:49:27 +0100

    button loader and show password

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,15 +6,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { JoingameComponent } from './joingame/joingame.component';
-import { LoadingComponent } from '../loading/loading/loading.component';
 
 @NgModule({
-  declarations: [
-    LogInComponent,
-    SignUpComponent,
-    JoingameComponent,
-    LoadingComponent,
-  ],
+  declarations: [LogInComponent, SignUpComponent, JoingameComponent],
   imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, FormsModule],
 })
 export class AuthModule {}
